7ae9977d90e8cf78761d75cf7b9079aba5f1af62
Aditya <dtrg21@gmail.com>
2020-03-30 18:16:09 +0530

    Update Login Page to become more attractive

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,6 +1,8 @@
 import 'package:dr_kirana/services/authservice.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pinput/pin_put/pin_put.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -17,50 +19,113 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Dr Kirana")
-      ),
-      body: Form(
-        key: formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 25.0, right: 25.0),
-              child: TextFormField(
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(hintText: 'Enter Phone Number'),
-                onChanged: (val) {
-                  setState(() {
-                    this.phoneNo = val;
-                  });
-                }
+      body: CustomPaint(
+        painter: BluePainter(),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(25, 0, 0, 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'डॉक्टर',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 60
+                      )
+                    ),
+                    Text(
+                      'किराणा',
+                      style: TextStyle(
+                        color: Colors.limeAccent,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 60
+                      )
+                    )
+                  ],
+                )
               ),
-            ),
-            codeSent ? Padding(
-              padding: EdgeInsets.only(left: 25.0, right: 25.0),
-              child: TextFormField(
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(hintText: 'Enter OTP'),
-                  onChanged: (val) {
-                    setState(() {
-                      this.smsCode = val;
-                    });
-                  }
+              Padding(
+                padding: EdgeInsets.only(left: 25.0, right: 25.0),
+                child: Stack(
+                  alignment: Alignment(1.1,0.0),
+                  children: <Widget>[
+                    TextFormField(
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(90),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: 'Enter Phone Number',
+                        prefixText: '+91',
+                      ),
+                      onChanged: (val) {
+                        setState(() {
+                          this.phoneNo = val;
+                        });
+                      }
+                    ),
+                    codeSent ? Container()
+                    : FlatButton(
+                        shape: new CircleBorder(),
+                        color: Colors.blueAccent,
+
+                        textColor: Colors.white,
+                        child: Icon(
+                          Icons.send,
+                        ),
+                        onPressed: () {
+                          codeSent ? AuthService().signInWithOTP(smsCode, verificationId) : verifyPhone(phoneNo);
+                        },
+                      ),
+                  ]
+                )
               ),
-            ) : Container(),
-            Padding(
-              padding: EdgeInsets.only(left: 25.0, right: 25.0),
-              child: RaisedButton(
-                child: Center(child: codeSent ? Text('Login') : Text('Verify')),
-                onPressed: () {
-                  codeSent ? AuthService().signInWithOTP(smsCode, verificationId) : verifyPhone(phoneNo);
-                },
-              ),
-            )
-          ]
-        )
-      ),
+              codeSent ? Padding(
+                padding: EdgeInsets.only(left: 25.0, right: 25.0),
+                child: PinPut(
+                  actionButtonsEnabled: false,
+                  inputDecoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    counterText: '',
+                    contentPadding: EdgeInsets.only(left: 10, right: 10, top: 8.0, bottom: 8.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(3)
+                    )
+                  ),
+                  fieldsCount: 6,
+                  onSubmit: (String pin) {
+                    codeSent ? AuthService().signInWithOTP(pin, verificationId) : verifyPhone(phoneNo);
+                  },
+                )
+              ) : Container(),
+              codeSent ? Padding(
+                padding: EdgeInsets.only(left: 25.0, right: 25.0),
+                child: RaisedButton(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  textTheme: ButtonTextTheme.primary,
+                  textColor: Colors.black,
+                  child: Center(child: Text('Verify')),
+                  onPressed: () {
+                    codeSent ? AuthService().signInWithOTP(smsCode, verificationId) : verifyPhone(phoneNo);
+                  },
+                ),
+              ) : Container()
+            ]
+          )
+        ),
+      )
     );
   }
   
@@ -93,5 +158,44 @@ class _LoginPageState extends State<LoginPage> {
         codeSent: smsSent,
         codeAutoRetrievalTimeout: autoTimeout
     );
+  }
+}
+
+class BluePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final height = size.height;
+    final width = size.width;
+    Paint paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
+    paint.color = Colors.blue.shade700;
+    canvas.drawPath(mainBackground, paint);
+
+    Path ovalPath = Path();
+    // Start paint from 20% height to the left
+    ovalPath.moveTo(0, height * 0.2);
+
+    // paint a curve from current position to middle of the screen
+    ovalPath.quadraticBezierTo(
+        width * 0.45, height * 0.25, width * 0.51, height * 0.5);
+
+    // Paint a curve from current position to bottom left of screen at width * 0.1
+    ovalPath.quadraticBezierTo(width * 0.58, height * 0.8, width * 0.1, height);
+
+    // draw remaining line to bottom left side
+    ovalPath.lineTo(0, height);
+
+    // Close line to reset it back
+    ovalPath.close();
+
+    paint.color = Colors.blue.shade600;
+    canvas.drawPath(ovalPath, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
   }
 }
