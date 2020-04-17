9f779bd1ee033a4f9d063aca4cfc629beed3e4c7
Aditya <dtrg21@gmail.com>
2020-04-18 01:42:00 +0530

    Add shop menu checker

--- lib/screens/order/listcapture.dart
+++ lib/screens/order/listcapture.dart
@@ -1,14 +1,18 @@
 import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dr_kirana/screens/order/image_view.dart';
 import 'package:dr_kirana/screens/order/uploader.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
-
 import 'package:image_picker/image_picker.dart';
 
 class ListCapturePage extends StatefulWidget {
-  final String type, shop, note;
+  final DocumentSnapshot shop;
+  final String type;
 
-  ListCapturePage ({ Key key, @required this.type, @required this.shop, @required this.note }) : super(key: key);
+  ListCapturePage({Key key, @required this.type, @required this.shop})
+      : super(key: key);
 
   @override
   _ListCapturePageState createState() => _ListCapturePageState();
@@ -21,13 +25,12 @@ class _ListCapturePageState extends State<ListCapturePage> {
   Future<File> _checkIfPaper(File image, BuildContext context) async {
     final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(image);
     bool status;
-    final ImageLabeler labeler = FirebaseVision.instance.imageLabeler(
-      ImageLabelerOptions(confidenceThreshold: 0.65)
-    );
+    final ImageLabeler labeler = FirebaseVision.instance
+        .imageLabeler(ImageLabelerOptions(confidenceThreshold: 0.65));
     final List<ImageLabel> labels = await labeler.processImage(visionImage);
 
     for (ImageLabel label in labels) {
-      if(label.text == 'Paper') {
+      if (label.text == 'Paper') {
         status = true;
       } else {
         status = false;
@@ -38,17 +41,16 @@ class _ListCapturePageState extends State<ListCapturePage> {
   }
 
   showAlertDialog(BuildContext context) {
-
     // set up the buttons
     Widget cancelButton = FlatButton(
       child: Text("Cancel"),
-      onPressed:  () {
+      onPressed: () {
         Navigator.pop(context);
       },
     );
     Widget continueButton = FlatButton(
       child: Text("पुन्हा प्रयत्न करा"),
-      onPressed:  () {
+      onPressed: () {
         Navigator.pop(context);
         _captureImage(context);
       },
@@ -57,7 +59,8 @@ class _ListCapturePageState extends State<ListCapturePage> {
     // set up the AlertDialog
     AlertDialog alert = AlertDialog(
       title: Text("ERROR"),
-      content: Text("आपण वापरलेल्या फोटोमध्ये यादी दिसून येत नाही. कृपया पुन्हा प्रयत्न करा."),
+      content: Text(
+          "आपण वापरलेल्या फोटोमध्ये यादी दिसून येत नाही. कृपया पुन्हा प्रयत्न करा."),
       actions: [
         cancelButton,
         continueButton,
@@ -74,7 +77,8 @@ class _ListCapturePageState extends State<ListCapturePage> {
   }
 
   Future<void> _captureImage(BuildContext context) async {
-    File selected = await ImagePicker.pickImage(source: ImageSource.camera, imageQuality: 50);
+    File selected = await ImagePicker.pickImage(
+        source: ImageSource.camera, imageQuality: 50);
 
     File image = await _checkIfPaper(selected, context);
 
@@ -90,69 +94,92 @@ class _ListCapturePageState extends State<ListCapturePage> {
         title: new Text("ऑर्डर करा"),
       ),
       body: Center(
-        child: Center(
-          child:  Column(
-            children: <Widget>[
-              _imageFile == null
-                ? Center(
-                  child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
-                        elevation: 5,
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: ListTile(
-                                leading: Icon(Icons.info, color: Colors.blue),
-                                title: Text(
-                                  widget.note,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20
+          child: Center(
+              child: Column(children: <Widget>[
+        _imageFile == null
+            ? Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                          elevation: 5,
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ListTile(
+                                  leading: Icon(Icons.info, color: Colors.blue),
+                                  title: Text(
+                                    widget.shop["note"],
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
                                   ),
                                 ),
                               ),
-                            ),
-                            Container(
-                              color: Colors.green,
-                              child: ListTile(
-                                title: Text("आपल्या यादीची फोटो काढा", style: TextStyle(color: Colors.white),),
-                                leading: Icon(Icons.camera_alt, color: Colors.white,),
-                                onTap: () {
-                                  _captureImage(context);
-                                },
+                              if (widget.shop['menu'] != null)
+                                Container(
+                                  color: Colors.blue,
+                                  child: ListTile(
+                                    title: Text(
+                                      "आजचे भावफलक पहा",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    leading: Icon(
+                                      Icons.restaurant_menu,
+                                      color: Colors.white,
+                                    ),
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => ImageView(
+                                                  imageLocation:
+                                                      widget.shop['menu'])));
+                                    },
+                                  ),
+                                ),
+                              Container(
+                                color: Colors.green,
+                                child: ListTile(
+                                  title: Text(
+                                    "आपल्या यादीची फोटो काढा",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  leading: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.white,
+                                  ),
+                                  onTap: () {
+                                    _captureImage(context);
+                                  },
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    FlatButton.icon(
-                      label: Text("दुसरे दुकान निवडा"),
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      }
-                    ),
-                  ]
-                  ),
-                ) : Container(
-                  child: Image.file(
-                    _imageFile,
-                    height: MediaQuery.of(context).size.height/1.5,
-                    width: MediaQuery.of(context).size.width/1.5,
-                  ),
+                      FlatButton.icon(
+                          label: Text("दुसरे दुकान निवडा"),
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                    ]),
+              )
+            : Container(
+                child: Image.file(
+                  _imageFile,
+                  height: MediaQuery.of(context).size.height / 1.5,
+                  width: MediaQuery.of(context).size.width / 1.5,
                 ),
-              Uploader(file: _imageFile, type: widget.type, shop: widget.shop)
-            ]
-          )
-        )
-      ),
+              ),
+        Uploader(
+            file: _imageFile, type: widget.type, shop: widget.shop.documentID)
+      ]))),
     );
   }
 }
--- lib/screens/order/shop_selection.dart
+++ lib/screens/order/shop_selection.dart
@@ -19,129 +19,134 @@ class _ShopSelectionPageState extends State<ShopSelectionPage> {
   Widget build(BuildContext context) {
     var user = Provider.of<FirebaseUser>(context, listen: true);
 
-    if(user == null) {
+    if (user == null) {
       return Container(
         child: CircularProgressIndicator(),
       );
     }
 
     return Scaffold(
-      appBar: new AppBar(
-        title: Text("दुकान निवड करा")
-      ),
-      body: StreamBuilder(
-        stream: Firestore.instance.collection('users').document(user.uid).snapshots(),
-        builder: (context, userSnapshot) {
-          if(!userSnapshot.hasData)
-            return Container(
-                child: Center(
-                  child: CircularProgressIndicator(),
-                )
-            );
-          return StreamBuilder(
-              stream: listShops(userSnapshot),
-              builder: (context, snapshot) {
-                if(!snapshot.hasData)
-                  return Container(
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      )
-                  );
-                return GridView.builder(
-                    itemCount: snapshot.data.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                    ),
-                    itemBuilder: (context, index) {
-                      return Padding(
-                          padding: EdgeInsets.all(5),
-                          child: Card(
-                              elevation: 5,
-                              child: new InkWell(
-                                  child: Stack(
-                                      children: <Widget>[
-                                        Image(
-                                          image: NetworkImage(snapshot.data[index]['logo']),
-                                          fit: BoxFit.cover,
-                                          width: MediaQuery.of(context).size.width,
-                                          height: 200,
-                                        ),
-                                        Positioned.fill(
-                                          child: Opacity(
-                                            opacity: 0.8,
-                                            child: Container(
-                                              decoration: BoxDecoration(
-                                                gradient: LinearGradient(
-                                                    begin: Alignment.bottomCenter,
-                                                    end: Alignment.topCenter,
-                                                    colors: [Colors.black, Colors.black54, Colors.transparent]
-                                                ),
-                                              ),
+        appBar: new AppBar(title: Text("दुकान निवड करा")),
+        body: StreamBuilder(
+          stream: Firestore.instance
+              .collection('users')
+              .document(user.uid)
+              .snapshots(),
+          builder: (context, userSnapshot) {
+            if (!userSnapshot.hasData)
+              return Container(
+                  child: Center(
+                child: CircularProgressIndicator(),
+              ));
+            return StreamBuilder(
+                stream: listShops(userSnapshot),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData)
+                    return Container(
+                        child: Center(
+                      child: CircularProgressIndicator(),
+                    ));
+                  return GridView.builder(
+                      itemCount: snapshot.data.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      ),
+                      itemBuilder: (context, index) {
+                        return Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Card(
+                                elevation: 5,
+                                child: new InkWell(
+                                    child: Stack(children: <Widget>[
+                                      Image(
+                                        image: NetworkImage(snapshot.data[index]
+                                                ['logo'] ??
+                                            "https://lh3.googleusercontent.com/iCM0YSo2WRcOJDuq-oi6zB6ElFTJGL1kxbZsB9x6t3nQ2febwMSvVjh7c6N4JJJaZQ=s180"),
+                                        fit: BoxFit.cover,
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        height: 200,
+                                      ),
+                                      Positioned.fill(
+                                        child: Opacity(
+                                          opacity: 0.8,
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              gradient: LinearGradient(
+                                                  begin: Alignment.bottomCenter,
+                                                  end: Alignment.topCenter,
+                                                  colors: [
+                                                    Colors.black,
+                                                    Colors.black54,
+                                                    Colors.transparent
+                                                  ]),
                                             ),
                                           ),
                                         ),
-                                        Positioned.fill(
-                                          child: Column(
-                                              mainAxisAlignment: MainAxisAlignment.end,
-                                              crossAxisAlignment: CrossAxisAlignment.center,
-                                              children: <Widget>[
-                                                Padding(
-                                                  padding: EdgeInsets.all(3),
-                                                  child: Text(
-                                                      snapshot.data[index]['name'],
-                                                      textAlign: TextAlign.center,
-                                                      style: TextStyle(
-                                                        fontSize: 20,
-                                                        color: Colors.white,
-                                                        fontWeight: FontWeight.bold,
-                                                      )
-                                                  ),
-                                                ),
-                                                Padding(
-                                                  padding: EdgeInsets.all(3),
-                                                  child: Text(
-                                                      snapshot.data[index]['address'],
-                                                      style: TextStyle(
-                                                        fontSize: 16,
-                                                        color: Colors.white70,
-                                                      )
-                                                  ),
-                                                ),
-
-                                              ]
-                                          ),
-                                        ),
-                                      ]
-                                  ),
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => ListCapturePage(type: widget.type, shop: snapshot.data[index].documentID, note: snapshot.data[index]["note"])
-                                        )
-                                    );
-                                  }
-                              )
-                          )
-                      );
-                    }
-                );
-              }
-          );
-        },
-      )
-    );
+                                      ),
+                                      Positioned.fill(
+                                        child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: <Widget>[
+                                              Padding(
+                                                padding: EdgeInsets.all(3),
+                                                child: Text(
+                                                    snapshot.data[index]
+                                                        ['name'],
+                                                    textAlign: TextAlign.center,
+                                                    style: TextStyle(
+                                                      fontSize: 20,
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    )),
+                                              ),
+                                              Padding(
+                                                padding: EdgeInsets.all(3),
+                                                child: Text(
+                                                    snapshot.data[index]
+                                                        ['address'],
+                                                    style: TextStyle(
+                                                      fontSize: 16,
+                                                      color: Colors.white70,
+                                                    )),
+                                              ),
+                                            ]),
+                                      ),
+                                    ]),
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ListCapturePage(
+                                                      type: widget.type,
+                                                      shop: snapshot
+                                                          .data[index])));
+                                    })));
+                      });
+                });
+          },
+        ));
   }
 
   Stream<List<DocumentSnapshot>> listShops(AsyncSnapshot snapshot) {
     Geoflutterfire geo = Geoflutterfire();
-    GeoFirePoint center = geo.point(latitude: snapshot.data['location']['geopoint'].latitude, longitude: snapshot.data['location']['geopoint'].longitude);
-    var collectionReference = Firestore.instance.collection('shops').where('type', arrayContains: widget.type);
+    GeoFirePoint center = geo.point(
+        latitude: snapshot.data['location']['geopoint'].latitude,
+        longitude: snapshot.data['location']['geopoint'].longitude);
+    var collectionReference = Firestore.instance
+        .collection('shops')
+        .where('type', arrayContains: widget.type);
 
     double radius = 5;
     String field = 'location';
 
-    Stream<List<DocumentSnapshot>> stream = geo.collection(collectionRef: collectionReference)
+    Stream<List<DocumentSnapshot>> stream = geo
+        .collection(collectionRef: collectionReference)
         .within(center: center, radius: radius, field: field);
     return stream;
   }
